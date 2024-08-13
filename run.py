d2dd5aea1c8ba5935d759270b2eb73b8e6f32fff
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-08-13 11:04:22 +0000

    Create main function for the budget tracker

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def main():
+    """
+    Runs the main function for the app
+    """
+    print("Welcome to your budget tracker")
+    original_budget = float(input("Please enter your original spending budget: "))
+    budget = original_budget
+    spendings = []
+
+    while True:
+        print("\n Choose what do you want to do.")
+        print("1. Add spending")
+        print("2. Display budget details")
+        print("3. Exit")
+        choice = input("Enter you choice (1/2/3): ")
+
+        if choice == "1":
+            description = input("Add spending description: ")
+            amount = float(input("Enter spent amount: "))
+            add_spending(spendings, description, amount)
+
+        elif choice == "2":
+            display_budget_details(budget, spendings)
+        
+        elif choice == "3":
+            print("You are exiting your budget tracker. Goodbye!")
+        break
+        
+        else:
+        print("Invalid choice. Please choose one of the following options again (1/2/3).")
